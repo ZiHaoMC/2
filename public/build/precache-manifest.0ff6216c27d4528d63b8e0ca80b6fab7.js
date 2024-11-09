8bb3d657b0e8f0b44ba98577a7c78a1a28b0a983
ZiHaoMC <1532437323@qq.com>
2024-11-09 07:35:43 +0000

    first commit

---
+++ public/build/precache-manifest.0ff6216c27d4528d63b8e0ca80b6fab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c795e0123ce1aca5f76bb2635e3b2892",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b4c74bf125cb4168a59",
+    "url": "/static/css/2.29c1ed91.chunk.css"
+  },
+  {
+    "revision": "33c4514944d1f4363e6a",
+    "url": "/static/css/main.b93c1259.chunk.css"
+  },
+  {
+    "revision": "4b4c74bf125cb4168a59",
+    "url": "/static/js/2.19043fa8.chunk.js"
+  },
+  {
+    "revision": "33c4514944d1f4363e6a",
+    "url": "/static/js/main.523b76f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
